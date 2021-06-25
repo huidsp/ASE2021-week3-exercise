f7b766ee64b3cdebe335a7915fc479b780b76738
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2021-06-25 07:36:01 +0000

    task5-4

--- roman2arabic.go
+++ roman2arabic.go
@@ -6,6 +6,13 @@ func init() {
 	r2adata = make(map[string]int)
 	r2adata["I"] = 1
 	r2adata["II"] = 2
+	r2adata["III"] = 3
+	r2adata["IV"] = 4
+	r2adata["V"] = 5
+	r2adata["VI"] = 6
+	r2adata["VII"] = 7
+	r2adata["VIII"] = 8
+	r2adata["IX"] = 9
 }
 
 func r2a(x string) int {
--- roman2arabic_test.go
+++ roman2arabic_test.go
@@ -17,3 +17,13 @@ func TestR2A02(t *testing.T) {
 		t.Errorf("Expected %v, Result %v", expected, result)
 	}
 }
+
+func TestR2A03(t *testing.T) {
+	expected := []int{3, 4, 5, 6, 7, 8, 9}
+	input := []string{"III", "IV", "V", "VI", "VII", "VIII", "IX"}
+	for i, x := range input {
+		if expected[i] != r2a(x) {
+			t.Errorf("Expected %v, Result %v", expected[i], x)
+		}
+	}
+}
